1a18bf40d58c354bbb324ac41f589b6f2a586fb4
Cristhian Traversi <crist.trav@gmail.com>
2014-05-18 23:28:10 -0400

    Creado modelo para almacenar las etiquetas y puertos habilitados.

--- control/models.py
+++ control/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+#Modelo que representa los datos de los puertos habilitados guardados en la base de datos
+class configPuerto(models.Model): 
+    nropuerto = models.IntegerField()
+    descripcion = models.CharField(max_length=50)
