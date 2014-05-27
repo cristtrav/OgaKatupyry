67790f16e19f2f320432c0806cb4fd8d56ace40a
Cristhian Traversi <crist.trav@gmail.com>
2014-05-27 13:49:18 -0400

    Cambiado el modelo configPuerto. Campos actuales: puertoon,puertooff,descripcion.

--- control/models.py
+++ control/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 #Modelo que representa los datos de los puertos habilitados guardados en la base de datos
 class configPuerto(models.Model): 
-    nropuerto = models.IntegerField()
+    puertoon = models.IntegerField()
+    puertooff = models.IntegerField()
     descripcion = models.CharField(max_length=50)
