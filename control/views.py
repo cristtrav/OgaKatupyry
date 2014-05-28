52cb5f39f34b8a2d07e311939ab60f2182744670
Cristhian Traversi <crist.trav@gmail.com>
2014-05-28 16:10:09 -0400

    Agregados comentarios de referencia en la vista control

--- control/views.py
+++ control/views.py
@@ -18,35 +18,36 @@ def control(request):
     return render(request, "control.html", {"puertos" : cfgpuertosbd,})
 
 def accionarControl(request):
-    print("accionarControl llamado")    
+    print("accionarControl llamado")
+    #Se comprueba que el POST tenga los dos parametros necesarios
     if request.POST.has_key('numeroPuerto') and request.POST.has_key('idregistro'):
         nrp = int(request.POST.get('numeroPuerto'))#Se guarda el puerto actual a operar
-        idreg = int(request.POST.get('idregistro'))
+        idreg = int(request.POST.get('idregistro'))#Se guarda el id del registro de trabajo
         
         print ("Numero puerto recibido: "+str(nrp))#Se imprime el numero de puerto recibido
         print ("id recibido: "+str(idreg))#Se imprime el numero de puerto recibido
         
+        #Se carga el registro de la base de datos segun el codigo enviado
         regActual = configPuerto.objects.filter(id = idreg).first()
         
-        print ("Id obtenido de la bd: "+str(regActual.id))
+        print ("Id obtenido de la bd: "+str(regActual.id))#Se imprime el ID recibido
         
-        if(nrp==regActual.puertoon):
-            regActual.ultestado = True
-            regActual.save()
-        elif(nrp == regActual.puertooff):
-            regActual.ultestado = False
-            regActual.save()
+        if(nrp==regActual.puertoon):#Se comprueba si la accion es ON
+            regActual.ultestado = True #Se actualiza el dato
+            regActual.save() #Se guarda en la base de datos
+        elif(nrp == regActual.puertooff):# Se comprueba si la accion es OFF
+            regActual.ultestado = False #se actualiza el dato
+            regActual.save()#Se guarda en la base de datos
         
-        if(gpio_disponible):
-            GPIO.output(nrp, GPIO.LOW)
-            time.sleep(10)
-            GPIO.output(nrp, GPIO.HIGH)
-        else:
+        if(gpio_disponible):#Se comprueba si la libreria RPi.GPIO esta instalada
+            GPIO.output(nrp, GPIO.LOW)#Se activa la salida correspondiente
+            time.sleep(2)#Se pausa el programa 2 segundos
+            GPIO.output(nrp, GPIO.HIGH)#Se desactiva la salida correspondiente
+        else:#En caso de no estar instalada la libreria RPi.GPIO
             print("No se puede activar el puerto: RPi.GPIO no instalado")
         infoPuerto = {#Se crea el objeto JSON para enviar a la pagina
             "puertoActual": nrp,
         }
-        time.sleep(2)
         return HttpResponse(json.dumps(infoPuerto), content_type="application/json")#Se envia el objeto JSON
     else:#Ocurre si no se eviaron datos para un puerto
         print("No se encontraron todas las claves en el post")
